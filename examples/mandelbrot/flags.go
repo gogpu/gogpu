3b56243db4ddc952aabbace8dea4f416ba6f2a3b
forhammer <138378188+jbunds@users.noreply.github.com>
2026-08-11 10:11:10 +0200

    docs(examples): add Mandelbrot fractal renderer demo (#438)
    * docs(examples): add a Mandelbrot set fractal renderer demo (#418)
    
    * Fix compilation errors by adding go.mod and go.sum files to the package.
    
    Fix golangci-lint errors.
    
    Reformat all Go sourcecode with gofmt.
    
    * Apply all changes suggested by @kolkov per https://github.com/gogpu/gogpu/pull/438#issuecomment-5237075019
    
    Refactor the draw() method since it was arguably too long.
    
    * upgrade to github.com/gogpu/gogpu@latest and github.com/gogpu/gg@latest:
    
      $ go get github.com/gogpu/gogpu@latest
      go: downloading github.com/gogpu/gogpu v0.52.0
      go: downloading github.com/gogpu/gpucontext v0.27.0
      go: downloading github.com/gogpu/wgpu v0.31.0
      go: upgraded github.com/gogpu/gogpu v0.50.2 => v0.52.0
      go: upgraded github.com/gogpu/gpucontext v0.24.0 => v0.27.0
      go: upgraded github.com/gogpu/wgpu v0.30.37 => v0.31.0
    
      $ go get github.com/gogpu/gg@latest
      go: downloading github.com/gogpu/gg v0.52.1
      go: upgraded github.com/gogpu/gg v0.50.14 => v0.52.1

---
+++ examples/mandelbrot/flags.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+// coords represents the named x, y coordinates of a target point of interest.
+type coords struct {
+	name string
+	x, y float64
+}
+
+// pointsOfInterest returns a slice of named x, y coordinates of canonical points of interest in the Mandelbrot set.
+func pointsOfInterest() map[string]coords {
+	return map[string]coords{
+		"spiral":    {name: "spiral", x: -0.088, y: 0.6555}, // default
+		"elephant":  {name: "elephant", x: 0.2777, y: 0.0073},
+		"lightning": {name: "lightning", x: -1.25066, y: 0.02012},
+		"seahorse":  {name: "seahorse", x: -0.743643887037151, y: 0.131825904205330},
+		"scepter":   {name: "scepter", x: -1.45, y: 0.0},
+		"spider":    {name: "spider", x: -1.4063, y: 0.0},
+	}
+}
+
+// flags parses command line flags and returns the named x, y coordinates of the user-specified point of interest.
+func flags(fs *flag.FlagSet, args []string) (coords, error) {
+	args = filterArgs(args)
+	fs.Usage = func() {
+		fmt.Fprintf(fs.Output(), "%s usage:\n\n", filepath.Base(fs.Name()))
+		fs.PrintDefaults()
+		fmt.Fprintln(fs.Output())
+	}
+	var name string
+	fs.StringVar(&name, "target", "spiral", "canonical name for target x, y coordinates (seahorse, spiral, etc)")
+	if err := fs.Parse(args); err != nil {
+		fs.Usage()
+		return coords{}, err
+	}
+	points := pointsOfInterest()
+	target, ok := points[name]
+	if !ok {
+		fs.Usage()
+		fmt.Fprintf(os.Stderr, "valid values for -target:\n\n")
+		for name := range points {
+			fmt.Fprintf(os.Stderr, "  %s\n", name)
+		}
+		fmt.Fprintln(os.Stderr)
+		return coords{}, fmt.Errorf("invalid target specified: %q", name)
+	}
+	if len(fs.Args()) > 0 {
+		fmt.Fprintf(fs.Output(), "ignored arguments: %s\n", strings.Join(fs.Args(), ", "))
+	}
+	return target, nil
+}
+
+// filterArgs discards any arguments up to and including "--".
+func filterArgs(args []string) []string {
+	for i, arg := range args {
+		if arg == "--" {
+			args = args[i+1:]
+			break
+		}
+	}
+	return args
+}
